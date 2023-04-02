56cc174f7d96c54e21c7eb03d1eeda474a0ce096
vchabria <96632514+vchabria@users.noreply.github.com>
2023-04-01 23:54:21 -0400

    Update ClarifaiAPI.py

--- ClarifaiAPI.py
+++ ClarifaiAPI.py
@@ -5,30 +5,49 @@
 # pip install clarifai-grpc
 # pip install pandas
 # pip install pillow
-
+import os
+import openai
+from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
 import pandas as pd
+        
+load_dotenv()
+openai.api_key = os.getenv("APIkey")
 
-st.title("Nutri:orange[Ai]d")
+def chat(user_input):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"{user_input}",
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
 
-st.header("Introducing Nutri:orange[Ai]d – your ultimate nutrition partner! "
+    message = response.choices[0].text.strip()
+    return message
+
+
+
+st.title("NutriAid")
+
+st.header("Introducing NutriAid – your ultimate nutrition partner! "
           "Whether you’re aiming to hit specific macros or sticking to a meal budget, "
-          "Nutri:orange[Ai]d has got you covered. You can plan, cook, and track your meals with ease, "
+          "NutriAid has got you covered. Now you can Aid, you can plan, cook, and track your meals with ease, "
           "ensuring that you always stay on track towards your health goals.")
 
-st.markdown("Nutri:orange[Ai]d is your ultimate solution for achieving your health goals with a healthy and balanced diet. "
-        "With Nutri:orange[Ai]d, all you need to do is take a photo of your ingredients and answer a few questions asked by "
+st.markdown("NutriAid is your ultimate solution for achieving your health goals with a healthy and balanced diet. "
+        "With NutriAid, all you need to do is take a photo of your ingredients and answer a few questions asked by "
         "our AI-powered chatbot to get customized diet plans tailored to your specific dietary needs. "
-        "But Nutri:orange[Ai]d doesn’t stop there – it takes meal planning to the next level by providing macro tracking "
+        "But NutriAid doesn’t stop there – it takes meal planning to the next level by providing macro tracking "
         "and analysis of your daily food intake. Our chatbot helps you balance your macronutrients and "
         "gives insights into your nutritional habits, so you can stay on track with your health goals without "
         "having to spend hours searching for recipes. Say goodbye to boring meals and hello to a variety of tasty "
-        "and healthy dishes with Nutri:orange[Ai]d. Experience the future of food and nutrition right at your fingertips!")
+        "and healthy dishes with NutriAid. Experience the future of food and nutrition right at your fingertips!")
 
 # 2) Input Files
-st.header("Upload Image - jpg")
-file_data = st.file_uploader("", type=['jpg'])
+file_data = st.file_uploader("Upload Image - jpg", type=['jpg', 'jpeg'])
 
 if file_data == None:
     st.warning("Error: No file detected")
@@ -48,8 +67,11 @@ from clarifai_grpc.grpc.api.status import status_code_pb2
 
 CLARIFAI_API_KEY = "be9e7f2af35d416a8247cac074e8927e"
 APPLICATION_ID = "streamlit-app"
+# SAMPLE_URL = "https://samples.clarifai.com/metro-north.jpg"
 
 # This is how you authenticate.
+# metadata = (('authorization", f"Key'.format(key)),)
+# metadata = (('authorization', 'Key ' + CLARIFAI_API_KEY),)
 metadata = (("authorization", f"Key {CLARIFAI_API_KEY}"),)
 
 
@@ -74,19 +96,23 @@ confidences = []
 
 for concept in response.outputs[0].data.concepts:
     # st.write("%12s: %.2f" % (concept.name, concept.value))
-    if (concept.name != "no person"):
-        names.append(concept.name)
-        confidences.append(concept.value)
+    names.append(concept.name)
+    confidences.append(concept.value)
 
-# 4) Keywords found
+df = pd.DataFrame ({
+    "Concept Name":names,
+    "Model Confidence:":confidences
+})
+st.dataframe(df)
 
 # Keywords
 st.header("Keywords found:")
 x = ", ".join(names)
 st.write(x)
 
-# 5) Take output of keywords, plug into chatGPT
-image = Image.open('/Users/dcruz/PycharmProjects/HackPrinceton2023/ss1.jpg')
-if file_data != None:
-    st.header("Recipe using keywords:")
-    st.image(image)
+
+user_input = st.text_input("Ask Nutri:orange[Ai]d a question")
+if st.button("Submit"):
+    response = chat(user_input)
+    st.write(response)
+
