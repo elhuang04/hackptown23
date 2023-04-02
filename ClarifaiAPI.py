64b6386724bcfcf4009338fccd36ea5b6fa36e83
27davezurc <90871066+27davezurc@users.noreply.github.com>
2023-04-01 18:00:57 -0700

    Update ClarifaiAPI.py

--- ClarifaiAPI.py
+++ ClarifaiAPI.py
@@ -10,34 +10,27 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 
-st.title("NutriAid")
+st.set_page_config(page_title="My Streamlit App", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded", background_color="#f8f9fa")
 
-st.header("Introducing NutriAid – your ultimate nutrition partner! "
+st.title("Nutri:orange[Ai]d")
+
+st.header("Introducing Nutri:orange[Ai]d – your ultimate nutrition partner! "
           "Whether you’re aiming to hit specific macros or sticking to a meal budget, "
-          "NutriAid has got you covered. Now you can Aid, you can plan, cook, and track your meals with ease, "
+          "Nutri:orange[Ai]d has got you covered. You can plan, cook, and track your meals with ease, "
           "ensuring that you always stay on track towards your health goals.")
 
-st.markdown("NutriAid is your ultimate solution for achieving your health goals with a healthy and balanced diet. "
-        "With NutriAid, all you need to do is take a photo of your ingredients and answer a few questions asked by "
+st.markdown("Nutri:orange[Ai]d is your ultimate solution for achieving your health goals with a healthy and balanced diet. "
+        "With Nutri:orange[Ai]d, all you need to do is take a photo of your ingredients and answer a few questions asked by "
         "our AI-powered chatbot to get customized diet plans tailored to your specific dietary needs. "
-        "But NutriAid doesn’t stop there – it takes meal planning to the next level by providing macro tracking "
+        "But Nutri:orange[Ai]d doesn’t stop there – it takes meal planning to the next level by providing macro tracking "
         "and analysis of your daily food intake. Our chatbot helps you balance your macronutrients and "
         "gives insights into your nutritional habits, so you can stay on track with your health goals without "
         "having to spend hours searching for recipes. Say goodbye to boring meals and hello to a variety of tasty "
-        "and healthy dishes with NutriAid. Experience the future of food and nutrition right at your fingertips!")
-
-# 1) API Enter app key - COMMENTED OUT / NO NEED
-# st.header("Enter app key: ")
-# key = st.text_input("App Key")
-
-# if (key == ""):
-#     st.warning("Error: No app key detected")
-#     st.stop()
-# else:
-#     st.write("An app key has been uploaded")
+        "and healthy dishes with Nutri:orange[Ai]d. Experience the future of food and nutrition right at your fingertips!")
 
 # 2) Input Files
-file_data = st.file_uploader("Upload Image - jpg", type=['jpg', 'jpeg'])
+st.header("Upload Image - jpg")
+file_data = st.file_uploader("", type=['jpg'])
 
 if file_data == None:
     st.warning("Error: No file detected")
@@ -57,11 +50,8 @@ from clarifai_grpc.grpc.api.status import status_code_pb2
 
 CLARIFAI_API_KEY = "be9e7f2af35d416a8247cac074e8927e"
 APPLICATION_ID = "streamlit-app"
-# SAMPLE_URL = "https://samples.clarifai.com/metro-north.jpg"
 
 # This is how you authenticate.
-# metadata = (('authorization", f"Key'.format(key)),)
-# metadata = (('authorization', 'Key ' + CLARIFAI_API_KEY),)
 metadata = (("authorization", f"Key {CLARIFAI_API_KEY}"),)
 
 
@@ -86,11 +76,19 @@ confidences = []
 
 for concept in response.outputs[0].data.concepts:
     # st.write("%12s: %.2f" % (concept.name, concept.value))
-    names.append(concept.name)
-    confidences.append(concept.value)
+    if (concept.name != "no person"):
+        names.append(concept.name)
+        confidences.append(concept.value)
 
-df = pd.DataFrame ({
-    "Concept Name":names,
-    "Model Confidence:":confidences
-})
-st.dataframe(df)
+# 4) Keywords found
+
+# Keywords
+st.header("Keywords found:")
+x = ", ".join(names)
+st.write(x)
+
+# 5) Take output of keywords, plug into chatGPT
+image = Image.open('/Users/dcruz/PycharmProjects/HackPrinceton2023/ss1.jpg')
+if file_data != None:
+    st.header("Recipe using keywords:")
+    st.image(image)
